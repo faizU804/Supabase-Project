cb7bb7eeb075196857c142d8e6cb985bd380f633
Faiz Ur Rehman <159672492+faizU804@users.noreply.github.com>
2025-12-26 11:12:11 +0500

    changes in supabse-client.ts
    you need here to add your supabase secret keys directly in this file. then project will run perfectly

--- src/app/supabase-client.ts
+++ src/app/supabase-client.ts
@@ -1,4 +1,4 @@
 import {createClient} from "@supabase/supabase-js"
 
 // they need url and anonyms or secret key so we use it here below in createclient
-export const supabase = createClient("https://eyaxnlqgossudoezsdqk.supabase.co","sb_publishable_Nf-2a-RKPfTqn_g9aTl66Q_HuGhLyLx")
+export const supabase = createClient("do your supabase keys secret, and partial both here insted of env file ")
